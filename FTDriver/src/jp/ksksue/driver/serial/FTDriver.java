79ecfbba5c10fabcbfeba6fb1389c7258c74a5ba
ukuleletrip <ukuleletrip@gmail.com>
2011-12-12 17:51:04 +0900

    changed interfaces for supporting InputStream, OutputStream.

--- FTDriver/src/jp/ksksue/driver/serial/FTDriver.java
+++ FTDriver/src/jp/ksksue/driver/serial/FTDriver.java
@@ -20,133 +20,30 @@ import java.util.List;
 import android.content.Intent;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
-import android.hardware.usb.UsbEndpoint;
 import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
 import android.util.Log;
 
 class UsbId {
-	int mVid;
-	int mPid;
-	UsbId(int vid, int pid) { mVid = vid; mPid = pid; }
-}
-
-class FTSerialPort {
-	private final UsbInterface mInterface;
-	private final UsbDeviceConnection mDeviceConnection;
-	private final int mIntfNo;
-	private final UsbEndpoint mIn;
-	private final UsbEndpoint mOut;
-	
-    private static final String TAG = "FTSerialPort";
-
-	FTSerialPort(UsbInterface intf, UsbDeviceConnection connection, int intfNo) {
-    	if (!connection.claimInterface(intf, false)) {
-			throw new IllegalArgumentException(
-                	"interface " + intf.toString() + " failed to be claimed");
-    	}
-    	Log.d(TAG,"claim interface succeeded");
-    	Log.d(TAG,"interface ID : "+intf.getId());
-		mInterface = intf;
-		mIntfNo = intfNo;
-		mDeviceConnection = connection;
-		if (intf.getEndpointCount() < 2) {
-			throw new IllegalArgumentException(
-                    	"interface " + intf.toString() + " does not have 2 endpoints");
-		}
-		mIn = intf.getEndpoint(0);
-    	mOut = intf.getEndpoint(1);
-	}
-	public void end() {
-        mDeviceConnection.releaseInterface(mInterface);
-	}
-	public boolean open(int baudrate) {
-		initFTDIChip(baudrate, mIntfNo);
-		return true;
-	}
-	public void close() {
-		// do NOTHING.
-	}
-    // Read Binary Data
-    public int read(byte[] buf, int length) {
-    	byte[] rbuf = new byte[length+2];
-    	
-		int len = mDeviceConnection.bulkTransfer(mIn, rbuf, length+2, 0); // RX
-		if (len < 2) {
-			return -1;
-		}
-		// FIXME shift rbuf's pointer 2 to 0. (I don't know how to do.) 
-		for(int i=0; i<len; ++i) {
-			buf[i] = rbuf[i+2];
-		}
-		return (len-2);
-    }
-    // Write n byte Binary Data
-    public int write(byte[] buf,int length) {
-    	if(length > 64) {
-    		// [TODO] it is needed ?
-    		return -1;
-    	}
-		return mDeviceConnection.bulkTransfer(mOut, buf, length, 0); // TX    	
-    }
-    
-    // Initial control transfer
-	private void initFTDIChip(int baudrate, int interfaceNo) {
-		int baud = calcFTDIBaudrate(baudrate);
-		mDeviceConnection.controlTransfer(0x40, 0, 0, 0, null, 0, 0);				//reset
-		mDeviceConnection.controlTransfer(0x40, 0, 1, 0, null, 0, 0);				//clear Rx
-		mDeviceConnection.controlTransfer(0x40, 0, 2, 0, null, 0, 0);				//clear Tx
-		mDeviceConnection.controlTransfer(0x40, 0x02, 0x0000, 0, null, 0, 0);	//flow control none
-		mDeviceConnection.controlTransfer(0x40, 0x03, baud, 0, null, 0, 0);		//set baudrate
-		mDeviceConnection.controlTransfer(0x40, 0x04, 0x0008, 0, null, 0, 0);	//data bit 8, parity none, stop bit 1, tx off
-	}
-	
-	/* Calculate a Divisor at 48MHz
-	 * 9600	: 0x4138
-	 * 11400	: 0xc107
-	 * 19200	: 0x809c
-	 * 38400	: 0xc04e
-	 * 57600	: 0x0034
-	 * 115200	: 0x001a
-	 * 230400	: 0x000d
-	 */
-	private static int calcFTDIBaudrate(int baud) {
-		int divisor;
-		if(baud <= 3000000) {
-			divisor = calcFT232bmBaudBaseToDiv(baud, 48000000);
-		} else {
-			Log.e(TAG,"Cannot set baud rate : " + baud + ", because too high." );
-			Log.e(TAG,"Set baud rate : 9600" );
-			divisor = calcFT232bmBaudBaseToDiv(9600, 48000000);
-		}
-		return divisor;
-	}
-
-	// Calculate a divisor from baud rate and base clock for FT232BM, FT2232C and FT232LR
-	// thanks to @titoi2
-	private static int calcFT232bmBaudBaseToDiv(int baud, int base) {
-		int divisor;
-		divisor = (base / 16 / baud)
-		| (((base / 2 / baud) & 4) != 0 ? 0x4000 // 0.5
-				: ((base / 2 / baud) & 2) != 0 ? 0x8000 // 0.25
-						: ((base / 2 / baud) & 1) != 0 ? 0xc000 // 0.125
-								: 0);
-		return divisor;
-	}
+    int mVid;
+    int mPid;
+    UsbId(int vid, int pid) { mVid = vid; mPid = pid; }
 }
 
 public class FTDriver {
-    private static final UsbId[] IDS = { new UsbId(0x0403, 0x6001), new UsbId(0x0403, 0x6010) };
+    private static final UsbId[] IDS = { 
+        new UsbId(0x0403, 0x6001),
+        new UsbId(0x0403, 0x6010),
+    };
+    public static final int BAUD9600 = 9600;
+    public static final int BAUD14400 = 14400;
+    public static final int BAUD19200 = 19200;
+    public static final int BAUD38400 = 38400;
+    public static final int BAUD57600 = 57600;
+    public static final int BAUD115200 = 115200;
+    public static final int BAUD230400 = 230400;
 
-    public static final int BAUD9600	= 9600;
-    public static final int BAUD14400	= 14400;
-    public static final int BAUD19200	= 19200;
-    public static final int BAUD38400	= 38400;
-    public static final int BAUD57600	= 57600;
-    public static final int BAUD115200	= 115200;
-    public static final int BAUD230400	= 230400;
-
-    private static final String TAG = "FTDriver";
+    private static final String TAG = FTDriver.class.getSimpleName();
 
     private UsbManager mManager;
     private UsbDevice mDevice;
@@ -158,68 +55,33 @@ public class FTDriver {
         mPorts = new ArrayList<FTSerialPort>();
     }
     
+    public FTSerialPort getPort(int index) {
+        return mPorts.get(index);
+    }
+    
+    public FTSerialPort getPort() {
+        return getPort(0);
+    }
+
     // collect information of FTDI USB Device and create serial ports.
     public boolean begin() {
-    	for (UsbDevice device : mManager.getDeviceList().values()) {
-      	  	Log.i(TAG,"Devices : "+device.toString());
-      	  	if (beginDevice(device)) {
-      	  		return true;
-      	  	}
-    	}
-    	return false;
-    }
-    
-    // Open an FTDI USB Device
-    public boolean open(int portNo, int baudrate) {
-    	if (mPorts.size() <= portNo) {
-    		return false;
-    	}
-    	mPorts.get(portNo).open(baudrate);
-        return true;
-    }
-    public boolean open(int baudrate) {
-    	return open(0, baudrate);
-    }
-    
-    public void close(int portNo) {
-    	if (mPorts.size() <= portNo) {
-    		return;
-    	}
-    	mPorts.get(portNo).close();
-    }
-    public void close() {
-    	close(0);
+        for (UsbDevice device : mManager.getDeviceList().values()) {
+            Log.i(TAG, "Devices : " + device.toString());
+            if (beginDevice(device)) {
+                // in current version , we use only first device.
+                return true;
+            }
+        }
+        return false;
     }
 
     // Close the device
     public void end() {
-    	if (mDevice != null) {
-    		endDevice(mDevice);
-    	}
+        if (mDevice != null) {
+            endDevice(mDevice);
+        }
     }
 
-    // Read Binary Data
-    public int read(int portNo, byte[] buf, int length) {
-    	if (mPorts.size() <= portNo) {
-    		return -1;
-    	}
-    	return mPorts.get(portNo).read(buf, length);
-    }
-    public int read(byte[] buf) {
-    	return read(0, buf, buf.length);
-    }
-
-    // Write n byte Binary Data
-    public int write(int portNo, byte[] buf,int length) {
-    	if (mPorts.size() <= portNo) {
-    		return -1;
-    	}
-    	return mPorts.get(portNo).write(buf, length);
-    }
-    public int write(byte[] buf,int length) {
-    	return write(0, buf, length);
-    }
-    
     // TODO Implement these methods
 /*    public void available() {
     	
@@ -251,7 +113,7 @@ public class FTDriver {
         	return false;
         }
     	for (UsbInterface intf : intfs) {
-    		FTSerialPort port = createPort(device, connection, intf);
+    		FTSerialPort port = createPort(connection, intf);
     		if (port != null) {
     			newPorts.add(port);
     		}
@@ -273,9 +135,6 @@ public class FTDriver {
 		if (mDevice != null && mDevice.equals(deviceName)) {
 			Log.d(TAG, "USB interface removed");
 	        if (mDeviceConnection != null) {
-	        	for (FTSerialPort port : mPorts) {
-	        		port.end();
-	            }
 	        	mPorts.clear();
 	            mDeviceConnection.close();
 	            mDevice = null;
@@ -285,11 +144,9 @@ public class FTDriver {
     }
     
     // Sets the current USB device and interface
-    private FTSerialPort createPort(UsbDevice device, UsbDeviceConnection connection, UsbInterface intf) {
-    	Log.d(TAG,"Vendor ID : "+device.getVendorId());
-    	Log.d(TAG,"Product ID : "+device.getProductId());
+    private FTSerialPort createPort(UsbDeviceConnection connection, UsbInterface intf) {
     	try {
-    		return new FTSerialPort(intf, connection, intf.getId());
+    		return new FTSerialPort(connection, intf, intf.getId());
         } catch (Exception e) {
             Log.e(TAG, "error creating serial port", e);
         }
@@ -313,16 +170,16 @@ public class FTDriver {
     }
     
     // when insert the device USB plug into a USB port
-	public boolean usbAttached(Intent intent) {
-		Log.d(TAG, "USB device attached");
-		UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-		return beginDevice(device);
-	}
+    public boolean usbAttached(Intent intent) {
+        Log.d(TAG, "USB device attached");
+        UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+        return beginDevice(device);
+    }
 	
-	// when remove the device USB plug from a USB port
-	public void usbDetached(Intent intent) {
-		Log.d(TAG, "USB device detached");
-		UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-		endDevice(device);
-	}
+    // when remove the device USB plug from a USB port
+    public void usbDetached(Intent intent) {
+        Log.d(TAG, "USB device detached");
+        UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+        endDevice(device);
+    }
 }
---
+++ FTDriver/src/jp/ksksue/driver/serial/FTSerialPort.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright (C) 2011 @ksksue
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * 2011-12-12 modified by Ukuleletrip (@darkukll)
+ */
+package jp.ksksue.driver.serial;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
+import android.util.Log;
+
+class FTSerialInput extends InputStream {
+    private UsbDeviceConnection mDeviceConnection;
+    private UsbEndpoint mIn;
+    private int mMaxPacketSize;
+    private static final String TAG = FTSerialInput.class.getSimpleName();
+    
+    public FTSerialInput(UsbDeviceConnection conn, UsbEndpoint ep) {
+        mDeviceConnection = conn;
+        mIn = ep;
+        mMaxPacketSize = ep.getMaxPacketSize();
+        if (mMaxPacketSize == 0) {
+            mMaxPacketSize = 64;
+        }
+    }
+
+    @Override
+    public int read() throws IOException {
+        byte[] c = new byte[1];
+        if (read(c, 0, 1) == 1) {
+            return (int)c[0];
+        }
+        return -1;
+    }
+    
+    @Override
+    public int read (byte[] buffer, int offset, int length) {
+        byte[] rbuf = new byte[mMaxPacketSize];
+        int readLen = 0;
+
+        while (readLen < length) {
+            // we will get error (-1) if we call bulkTransfer with readLen 
+            // more than maxPacketSize and data in serial buffer more than it. 
+            int len = mDeviceConnection.bulkTransfer(mIn, rbuf,
+                    Math.min(mMaxPacketSize, length-readLen+2), 0);
+            if (len <= 2) {
+                Log.d(TAG, "read received " + len + " bytes.");
+                break;
+            }
+            // 1st and 2nd bytes are status bytes, they should be skipped.
+            // FIXME shift rbuf's pointer 2 to 0. (I don't know how to do.)
+            String dump = Integer.toHexString(rbuf[0]) + " " + Integer.toHexString(rbuf[1]) + " ";
+            for (int i = 0; i < len-2; ++i) {
+                buffer[offset + readLen + i] = rbuf[i + 2];
+                dump += Integer.toHexString(rbuf[i+2]) + " ";
+            }
+            Log.d(TAG, "read " + len + " bytes:" + dump);
+            readLen += (len - 2);
+        }
+        return readLen;
+    }
+}
+
+class FTSerialOutput extends OutputStream {
+    private UsbDeviceConnection mDeviceConnection;
+    private UsbEndpoint mOut;
+    private int mMaxPacketSize;
+    private static final String TAG = FTSerialInput.class.getSimpleName();
+    
+    public FTSerialOutput(UsbDeviceConnection conn, UsbEndpoint ep) {
+        mDeviceConnection = conn;
+        mOut = ep;
+        mMaxPacketSize = ep.getMaxPacketSize();
+        if (mMaxPacketSize == 0) {
+            mMaxPacketSize = 64;
+        }
+    }
+
+    @Override
+    public void write(int arg0) throws IOException {
+        byte[] c = new byte[1];
+        c[0] = (byte)arg0;
+        write(c, 0, 1);
+    }
+    
+    @Override
+    public void write (byte[] buffer, int offset, int count) throws IOException {
+        int writtenLen = 0;
+        byte[] writeBuffer = new byte[mMaxPacketSize];
+        while (writtenLen < count) {
+            int writeLen = Math.min(count - writtenLen, writeBuffer.length);
+            String dump = "";
+            for (int i=0; i < writeLen; i++) {
+                writeBuffer[i] = buffer[offset + writtenLen + i];
+                dump += Integer.toHexString(writeBuffer[i]) + " ";
+            }
+            Log.d(TAG, "write: " + dump);
+            int len;
+            if ((len = mDeviceConnection.bulkTransfer(mOut, writeBuffer, writeLen, 0)) < 0) {
+                throw new IOException("bulkTransfer returned " + len);
+            }
+            writtenLen += len;
+        }
+    }
+}
+
+public class FTSerialPort {
+    private static final String TAG = FTSerialPort.class.getSimpleName();
+    private UsbDeviceConnection mDeviceConnection;
+    private UsbInterface mInterface;
+    private FTSerialInput mInputStream;
+    private FTSerialOutput mOutputStream;
+    private int mInterfaceNo;
+    
+    public FTSerialPort(UsbDeviceConnection conn, UsbInterface intf, int intfNo) {
+        mDeviceConnection = conn;
+        mInterface = intf;
+        mInterfaceNo = intfNo;
+        if (mInterface.getEndpointCount() < 2) {
+            throw new IllegalArgumentException(
+                    "interface " + mInterface.toString() + " does not have 2 endpoints");
+        }
+        mInputStream = new FTSerialInput(mDeviceConnection, mInterface.getEndpoint(0));
+        mOutputStream = new FTSerialOutput(mDeviceConnection, mInterface.getEndpoint(1)); 
+    }
+    
+    public void open(int baudrate) {
+        if (!mDeviceConnection.claimInterface(mInterface, false)) {
+            throw new IllegalArgumentException(
+                    "interface " + mInterface.toString() + " failed to claim");
+        }
+        initFTDIChip(baudrate, mInterfaceNo);
+    }
+    
+    public void close() {
+        mDeviceConnection.releaseInterface(mInterface);
+    }
+    
+    public InputStream getInputStream() {
+        return mInputStream;
+    }
+    
+    public OutputStream getOutputStream() {
+        return mOutputStream;
+    }
+    
+    // Initial control transfer
+    private void initFTDIChip(int baudrate, int intfNo) {
+        int baud = calcFTDIBaudrate(baudrate);
+        mDeviceConnection.controlTransfer(0x40, 0, 0, intfNo, null, 0, 0);           //reset
+        mDeviceConnection.controlTransfer(0x40, 0, 1, intfNo, null, 0, 0);           //clear Rx
+        mDeviceConnection.controlTransfer(0x40, 0, 2, intfNo, null, 0, 0);           //clear Tx
+        mDeviceConnection.controlTransfer(0x40, 0x02, 0x0000, intfNo, null, 0, 0);   //flow control none
+        mDeviceConnection.controlTransfer(0x40, 0x03, baud, intfNo, null, 0, 0);     //set baudrate
+        mDeviceConnection.controlTransfer(0x40, 0x04, 0x0008, intfNo, null, 0, 0);   //data bit 8, parity none, stop bit 1, tx off
+    }
+    
+    /* Calculate a Divisor at 48MHz
+     * 9600 : 0x4138
+     * 11400    : 0xc107
+     * 19200    : 0x809c
+     * 38400    : 0xc04e
+     * 57600    : 0x0034
+     * 115200   : 0x001a
+     * 230400   : 0x000d
+     */
+    private static int calcFTDIBaudrate(int baud) {
+        int divisor;
+        if(baud <= 3000000) {
+            divisor = calcFT232bmBaudBaseToDiv(baud, 48000000);
+        } else {
+            Log.e(TAG,"Cannot set baud rate : " + baud + ", because too high." );
+            Log.e(TAG,"Set baud rate : 9600" );
+            divisor = calcFT232bmBaudBaseToDiv(9600, 48000000);
+        }
+        return divisor;
+    }
+
+    // Calculate a divisor from baud rate and base clock for FT232BM, FT2232C and FT232LR
+    // thanks to @titoi2
+    private static int calcFT232bmBaudBaseToDiv(int baud, int base) {
+        int divisor;
+        divisor = (base / 16 / baud) | (((base / 2 / baud) & 4) != 0 ? 0x4000 // 0.5
+                : ((base / 2 / baud) & 2) != 0 ? 0x8000 // 0.25
+                        : ((base / 2 / baud) & 1) != 0 ? 0xc000 // 0.125
+                                : 0);
+        return divisor;
+    }
+}
